ccebec35d2c6cd95a271b2b2e13dd47cc947eeb6
Brandon <103316367+okbrandon@users.noreply.github.com>
2024-12-10 17:41:05 +0100

    predictor: Data validation remade to seek at start

--- predictor.py
+++ predictor.py
@@ -5,23 +5,26 @@ class Predictor:
 		self.data = data
 
 	def validate_data(self):
-		columns = self.data.fieldnames
+		reader = csv.DictReader(self.data)
+		columns = reader.fieldnames
 
 		# Check if the column names are as expected
 		assert columns == ['km', 'price'], 'Column names are not as expected'
 
 		# Check if the data is as expected, float values, no missing values
-		for row in data:
+		for row in reader:
 			assert row['km'].replace('.', '', 1).isdigit(), 'km is not a float'
 			assert row['price'].replace('.', '', 1).isdigit(), 'price is not a float'
 
+		self.data.seek(0)
+
 if __name__ == '__main__':
 	input_file = 'data/data.csv'
-	data = csv.DictReader(open(input_file))
-	predictor = Predictor(data)
+	predictor = Predictor(open(input_file))
 
 	try:
 		predictor.validate_data()
+
 	except AssertionError as e:
 		print('Data is not valid:', e)
 	except Exception as e:
